193f22896a999808c35e558adcd9ef1cce49d106
ppardog <p.pardo.2022@alumnos.urjc.es>
2026-02-21 19:24:47 +0100

    [MOD] change api request filters (offset, rm select)

--- src/api.py
+++ src/api.py
@@ -2,9 +2,11 @@
 import os
 import requests
 from dotenv import load_dotenv
+import time
 load_dotenv()
 
-def call_endpoint(filters: dict) -> dict:
+
+def call_endpoint(filters: dict) -> list:
     """
     Llama al endpoint con los filtros proporcionados y devuelve los datos en formato JSON.
     """
@@ -12,25 +14,41 @@ def call_endpoint(filters: dict) -> dict:
     url = os.environ.get("ENDPOINT_URL")
     if not url:
         print("Error: ENDPOINT_URL environment variable is not set.")
-        return {}
+        return []
     
-    try:
-        response = requests.get(url, params=filters)
-        response.raise_for_status()  # launches an exception for HTTP error codes (400-599). For successful responses(200-299), the code continues to execute normally.
-        data = response.json()
-        return data
+    records = []
+    while True:
 
-    except requests.exceptions.HTTPError as eh:
-        print(f" HTTP Error: {eh}")
-        print (f"Server details {response.text}")
+        try:
+            response = requests.get(url, params=filters)
+            response.raise_for_status()  # launches an exception for HTTP error codes (400-599). For successful responses(200-299), the code continues to execute normally.
+            print(f"Retrieving data with offset {filters['offset']}...")
 
-    except requests.exceptions.ConnectionError as ec:
-        print(f"Internet/URL conexion error: {ec}")
-        
-    except requests.exceptions.Timeout as et:
-        print(f"Timeout error: {et}")
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Unexpected error when calling endpoint: {e}")
-        
-    return {}
+            data = response.json()
+            data_call=data.get("results", [])
+
+            # If the API returns an empty list of results, break the loop
+            if not data_call:
+                print("No more records to fetch...")
+                break
+            records.extend(data_call)
+
+            filters["offset"] += filters["limit"] 
+
+            time.sleep(0.5)
+
+        except requests.exceptions.HTTPError as eh:
+            print(f" HTTP Error: {eh}")
+            print (f"Server details {response.text}")
+
+        except requests.exceptions.ConnectionError as ec:
+            print(f"Internet/URL conexion error: {ec}")
+            
+        except requests.exceptions.Timeout as et:
+            print(f"Timeout error: {et}")
+            
+        except requests.exceptions.RequestException as e:
+            print(f"Unexpected error when calling endpoint: {e}")
+
+    print(f"Extraction completed! Sample length: {len(records)}")    
+    return records
--- src/main.py
+++ src/main.py
@@ -1,21 +1,28 @@
 #main.py
 from api import call_endpoint
-
+import pandas as pd
 """
 (Nota: Las APIs suelen poner un límite máximo total. En el caso de Melbourne, 
 si intentas pedir más de 10.000 registros usando limit+offset, te dará error. 
 Para descargas masivas, las APIs modernas tienen otra ruta que termina en /exports en lugar de /records).
 """
 
-
-
-
 #############
 filters = {
-    "limit": 10, 
-    "select": "kerbsideid, lastupdated, status_description", 
+    "limit": 100, 
+    "offset": 0,
+    #"select": "kerbsideid, lastupdated, status_description", 
     "order_by": "lastupdated desc" 
 }
-data=call_endpoint(filters)
-print(data)
-print(data['results'][0])
+results=call_endpoint(filters)
+
+if results:
+        # Lo pasamos a Pandas para verlo bonito y analizar las columnas
+        df_realtime = pd.DataFrame(results)
+        
+        print("\n--- COLUMNAS DISPONIBLES EN TIEMPO REAL ---")
+        df_realtime.info()
+        
+        print("\n--- VISTA PREVIA ---")
+        print(df_realtime.head(3))
+
