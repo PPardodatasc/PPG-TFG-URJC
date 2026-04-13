b31a3a9490d115dc724b7550bbe0f4d8d2e8166f
ppardog <p.pardo.2022@alumnos.urjc.es>
2026-04-13 23:53:48 +0200

    [MOD] include spatial data processing and export in EDA notebook

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,6 @@
 # utils.py
 import requests
+import pandas as pd
 
 def download_from_url(url: str, path: str) -> None:
     """
@@ -28,3 +29,31 @@ def download_from_url(url: str, path: str) -> None:
         print(f"Timeout error: {et}")
     except requests.exceptions.RequestException as e:
         print(f"Unexpected error when calling endpoint: {e}")
+
+
+
+def find_segment_coordinates(row, df_spatial):
+    """
+    Match the street and transversal streets from the sensor data with the road segment descriptions 
+    in the spatial dataset to find the corresponding latitude and longitude.
+    """
+    # Clean street names
+    street = str(row['StreetName']).lower().strip() if pd.notna(row['StreetName']) else ""
+    b1 = str(row['BetweenStreet1']).lower().strip() if pd.notna(row['BetweenStreet1']) else ""
+    b2 = str(row['BetweenStreet2']).lower().strip() if pd.notna(row['BetweenStreet2']) else ""
+        
+    # Possible patterns to match in the spatial dataset
+    pat1 = f"{street} between {b1} and {b2}"
+    pat2 = f"{street} between {b2} and {b1}"
+    
+    # Search for matches in the spatial dataset 
+    match = df_spatial[
+        df_spatial['desc_lower'].str.contains(pat1, regex=False) |
+        df_spatial['desc_lower'].str.contains(pat2, regex=False)
+    ]
+    
+    if not match.empty:
+        # If match, return latitude and longitude. Constant latitude and longitude for all sensors in the same segment.
+        return pd.Series([match.iloc[0]['latitude'], match.iloc[0]['longitude']])
+    
+    return pd.Series([None, None])
