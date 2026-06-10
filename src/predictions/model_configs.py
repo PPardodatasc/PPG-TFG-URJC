ce1080980653a30e6b91cb0aa0af25d8fc2306dc
ppardog <p.pardo.2022@alumnos.urjc.es>
2026-06-10 11:55:35 +0200

    [ADD] best csdi config + update hyperparam configs for csdi and saits

--- src/predictions/model_configs.py
+++ src/predictions/model_configs.py
@@ -2,23 +2,25 @@
 # TODO: This will be adapted with the best configurations found after hyperparameter tuning. For now, it contains the default configurations for all models.
 DEFAULT_MODEL_KWARGS = {
     # IMPUTATION MODELS
+    # Best params: {'window_size': 48, 'batch_size': 64, 'lr': 0.00047295728151429, 'n_layers': 2, 'd_model': 128, 'n_heads': 4, 'dropout': 0.2}
     'SAITS': {
         'epochs': 20,           
-        'window_size': 96,       
-        'batch_size': 32,        
-        'lr': 0.001,             
+        'window_size': 48,       
+        'batch_size': 64,        
+        'lr': 0.00047295728151429,             
         'n_layers': 2,
         'd_model': 128,
-        'n_heads': 2,
-        'dropout': 0.1,
+        'n_heads': 4,
+        'dropout': 0.2,
         'patience': 2,
         'model_saving_strategy': 'best'
     },
+    # Best params: {'window_size': 96, 'batch_size': 128, 'lr': 0.001289884852043542, 'n_layers': 3, 'n_channels': 128, 'n_heads': 2, 'dropout': 0.2}
     'CSDI': {
         'epochs': 20,
         'window_size': 96,
-        'batch_size': 32,
-        'lr': 0.001,
+        'batch_size': 128,
+        'lr': 0.001289884852043542,
         'n_layers': 3,
         'n_channels': 64,
         'n_heads': 2,
@@ -29,6 +31,7 @@ DEFAULT_MODEL_KWARGS = {
         'd_time_embedding': 128,
         'd_feature_embedding': 128,
         'd_diffusion_embedding': 128,
+        'dropout': 0.2
     },
 
     # FORECASTING MODELS
