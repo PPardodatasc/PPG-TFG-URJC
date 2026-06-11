0eac9fe336ff920d0ed36dd334d8d05a2ffb1276
ppardog <p.pardo.2022@alumnos.urjc.es>
2026-06-11 13:42:30 +0200

    [MOD] add best configs for forecasting models

--- src/predictions/model_configs.py
+++ src/predictions/model_configs.py
@@ -1,5 +1,4 @@
 # model_configs.py
-# TODO: This will be adapted with the best configurations found after hyperparameter tuning. For now, it contains the default configurations for all models.
 DEFAULT_MODEL_KWARGS = {
     # IMPUTATION MODELS
     # Best params: {'window_size': 48, 'batch_size': 64, 'lr': 0.00047295728151429, 'n_layers': 2, 'd_model': 128, 'n_heads': 4, 'dropout': 0.2}
@@ -34,28 +33,49 @@ DEFAULT_MODEL_KWARGS = {
         'dropout': 0.2
     },
 
-    # FORECASTING MODELS
-    'PatchTST': {
-        'n_heads': 4,
-        'd_model': 128,
-        'd_ffn': 256,
-        'patch_len': 16,     
-        'stride': 8,         
-        'dropout': 0.1,
-        'patience': 5,
+    #####  FORECASTING MODELS
+
+    # FORECASTING 24H, WINDOW SIZE 48H
+    # Best params: {'window_size':192, 'batch_size': 128, 'lr': 0.00376, 'n_layers': 2, 'd_model': 64, 'conv_kernel': [7, 9], 'dropout': 0.1}
+    'MICN_w192_ps96': {
+        'epochs': 20,
+        'window_size': 192,      
+        'd_model': 64,           
+        'n_layers': 2,           
+        'conv_kernel': [7, 9],   
+        'dropout': 0.1,          
+        'batch_size': 128,       
+        'lr': 0.00376,           
+        'patience': 2,
         'model_saving_strategy': 'best'
     },
-    'iTransformer': {
-        'd_model': 128,
-        'd_ffn': 256,
-        'n_heads': 4,
-        'dropout': 0.1,
-        'patience': 5,
+    # Best params: {'window_size': 192, 'batch_size': 32, 'lr': 0.00011, 'n_layers': 2, 'd_model': 256, 'n_heads': 8, 'dropout': 0.1}
+    'Transformer_w192_ps96': {
+        'epochs': 20,
+        'window_size': 192,      
+        'd_model': 256,          
+        'd_ffn': 256,           
+        'n_heads': 8,            
+        'n_layers': 2,           
+        'n_encoder_layers': 2,
+        'n_decoder_layers': 2,
+        'dropout': 0.1,          
+        'batch_size': 32,        
+        'lr': 0.00011,           
+        'patience': 2,
         'model_saving_strategy': 'best'
     },
-    'DLinear': {
-        'moving_avg_window_size': 25, 
-        'patience': 5,
+    # Best params: {'window_size': 192, 'batch_size': 128, 'lr': 0.00663, 'n_layers': 2, 'd_model': 128, 'conv_kernel': [7, 9], 'dropout': 0.1}
+    'DLinear_w192_ps96': {
+        'epochs': 30,
+        'window_size': 192,  
+        'moving_avg_window_size': 99,  
+        'd_model': 128,                
+        'batch_size': 128,            
+        'lr': 0.00663,                 
+        'patience': 2,
         'model_saving_strategy': 'best'
     }
+
+    # FORECASTING 48H, WINDOW SIZE 96H
 }
